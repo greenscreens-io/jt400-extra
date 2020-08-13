a068c80931c48fc2f7e64475794e81c278967337
unknown <greenscreens.ltd@gmail.com>
2020-08-13 10:15:54 +0200

    Format converter improvement, array support

--- io.greenscreens.jt400/src/main/java/io/greenscreens/jt400/annotations/JT400Format.java
+++ io.greenscreens.jt400/src/main/java/io/greenscreens/jt400/annotations/JT400Format.java
@@ -23,35 +23,35 @@ import java.lang.annotation.Target;
  * 
  * Examples defining date
  * 
- *  @JT400Format(offset = 101, type = AS400DataType.TYPE_DATE, format = AS400Date.FORMAT_CYMD)
- * 	protected double val;
+ *	@JT400Format(offset = 101, type = AS400DataType.TYPE_DATE, format = AS400Date.FORMAT_CYMD)
+ *	protected double val;
  * 
  * Examples defining time
  * 
- *  @JT400Format(offset = 101, type = AS400DataType.TYPE_TIME, format = AS400Time.FORMAT_EUR)
- * 	protected double val;
+ *	@JT400Format(offset = 101, type = AS400DataType.TYPE_TIME, format = AS400Time.FORMAT_EUR)
+ *	protected double val;
  * 
  * Examples defining dec-float
  * 
- *  @JT400Format(offset = 101, type = AS400DataType.TYPE_DECFLOAT, decimals = 5)
- * 	protected double val;
+ *	@JT400Format(offset = 101, type = AS400DataType.TYPE_DECFLOAT, decimals = 5)
+ *	protected double val;
  * 
- *  @JT400Format(offset = 101, type = AS400DataType.TYPE_DECFLOAT, decimals = 5)
- * 	protected BigDecimal val;
+ *	@JT400Format(offset = 101, type = AS400DataType.TYPE_DECFLOAT, decimals = 5)
+ *	protected BigDecimal val;
  *
  * Examples defining arrays
  * 
- * 	Array of string word 3 chars long, array size at field marked with offset 51
- * 	@JT400Format(offset = 101, length = 3, of = 51)
- * 	protected String[] val;
+ *	Array of string word 3 chars long, array size at field marked with offset 51
+ *	@JT400Format(offset = 101, length = 3, of = 51)
+ *	protected String[] val;
  * 
- *  Array of 10 integers
- *  @JT400Format(offset = 4, type = AS400DataType.TYPE_BIN4, length = 10)
- *  protected int[] val;
+ *	Array of 10 integers
+ *	@JT400Format(offset = 4, type = AS400DataType.TYPE_BIN4, length = 10)
+ *	protected int[] val;
  *  
- *  Array of integers, array size at field marked with offset 51
- *  @JT400Format(offset = 4, type = AS400DataType.TYPE_BIN4, of = 10)
- *  protected int[] val;
+ *	Array of integers, array size at field marked with offset 51
+ *	@JT400Format(offset = 4, type = AS400DataType.TYPE_BIN4, of = 10)
+ *	protected int[] val;
  *  
  */
 @Retention(RUNTIME)
